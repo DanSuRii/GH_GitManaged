770982e179338fadd39e3443305a0a49d5427ae0
박단 <dansuri2@gmail.com>
2019-01-29 08:32:01 +0100

    CI 2019 01 29

--- CPPTest/CPPTest/CtxConsistency.cpp
+++ CPPTest/CPPTest/CtxConsistency.cpp
@@ -7,6 +7,8 @@
 
 #include <vector>
 #include <thread>
+#include <queue>
+#include <list>
 
 template< class T, class ...Args >
 auto New( Args&& ...args )
@@ -27,6 +29,89 @@ void Log(Args&& ...args)
 
 #define LOG_FN( ... )  Log( __FUNCSIG__ , __VA_ARGS__ )
 
+#if false // it has no idea whats live in 
+class IBucket
+{
+public:
+	IBucket(int nID)
+		: _nID(nID)
+	{
+
+	}
+
+
+private:
+	int _nID = -1;
+};
+using PIBucket = std::shared_ptr<IBucket>;
+
+//POOLER for IOPool or BufferPool
+//It doesnt handle Client or somthing another, resources are never protect against asynchronous access.
+class POOLER
+{
+	const size_t DEFAULT_POOL_LEN = 256;
+	POOLER()
+	{
+
+	}
+public:
+
+	//create on access, dosnt need to populate at creation
+	PIBucket GetBucket(int idx)
+	{
+		if (idx < 0 || idx >= pool.size())
+			return nullptr;
+
+		if (pool[idx] == nullptr)
+		{
+			//Create from Request? it need to ..creation information
+		}
+	}
+
+	static POOLER& GetInstance() 
+	{
+		static POOLER _inst;
+		return _inst;
+	}
+private:
+	void Recycle(int idx)
+	{
+
+	}
+	//never removed in here, just for a reuse waiting
+	//and... this also doesnt need to Synchronise 
+	std::vector< PIBucket > pool{ DEFAULT_POOL_LEN };
+
+	//This just need to Synchronise. it doesnt need to search or loop.
+	std::list< unsigned int > available;
+} & g_pool = POOLER::GetInstance();
+
+enum EBucketTimes
+{
+	EBT_CREATIONAL,
+	EBT_LASTRECYCLED,
+	EBT_LASTMODIFIED,
+
+	EBT_CNT,
+};
+
+template< class T >
+class Bucket : public IBucket
+{
+public:
+	Bucket() {};
+	operator T&() { Access(); }
+	T& Access() { return _data; }
+private:
+	T _data;
+
+};
+
+void ClientCode()
+{
+}
+
+#endif // false
 
 class WSAInitializer 
 {
@@ -55,6 +140,8 @@ public:
 private:
 };
 using PIOCtx = std::shared_ptr<IOCtx>;
+
+//file io, network io, and...
 
 
 class ClientCtx
@@ -113,8 +200,79 @@ private:
 	size_t _threadCnt = 0;
 	std::vector< std::thread >	_threads;
 };
+using PIOCP = std::shared_ptr<IOCP>;
+
+//make it pooling, BUFFER or 
+
+
+
+template<class Obj>
+class POOL
+{
+public:
+	const int DEFAULT_CONTAINER_CNT;
+	using PObj = std::shared_ptr<Obj>;
+	
+	class Bucket
+	{
+	public:
+		Bucket( int pos )
+			: _pos(pos)
+		{
+		}
+	private:
+		int _pos;
+		PObj _data; //possible to char _data[sizeof(Obj)]; new(_data) Obj();
+	};
+	using PBucket = std::shared_ptr< Bucket >;
+
+	static POOL& GetInstance() {
+		static POOL _instance;
+		return _instance;
+	}
+private:
+	POOL()
+	{
+		for (int i = 0; i < _cont.size(); ++i)
+		{
+			_cont[i] = std::make_shared<Bucket>(i);
+			_available.push_back(i);
+		}
+	};
+
+	void _grow_up()
+	{
+		size_t oldSize = _cont.size();
+		_cont.resize(_cont.size() * 2);
+
+		for (size_t sz = oldSize; sz < _cont.size(); ++sz)
+		{
+			_available.push_back(sz);
+		}
+	}
+
+	std::vector<PBucket> _cont{ DEFAULT_CONTAINER_CNT };
+
+	std::list<int> _available;
+};
+
+POOL<IOCtx>::PObj NewIOctx()
+{
+
+}
+
+
+
+void ClientCode()
+{
+}
+
+
 
 IMPL_TEST_FNC(CTX_CONSISTENCY)
 {
+	//POOLER pooler;
 	IOCP iocp(8);
+	std::vector<int> vecInt{ 1,2,3 };
+	vecInt.push_back(3);
 }
